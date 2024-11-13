871c13e916c6cec1cecb13ec045e307011665a14
thunwachan <sittankunanit2008@gmail.com>
2024-11-13 19:35:43 +0700

    auth button (nf)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,20 +4,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <div className="bg-indigo-300 mx-10 font-bold">
-        <h1 className="text-center bold">ทำกฎหมายให้เป็นเรื่องง่าย!</h1>
-      </div>
-      <div>
-        <h1>กฎหมาย</h1>
-      </div>
-      <div>
-        <div>
-          <h1>กฎหมายอาญา</h1>
-        </div>
-        <div>
-          <h1>กฎหมายแพ่งและพาณิชย์</h1>
-        </div>
-      </div>
+      kub
+      
+
     </div>
   );
 }
