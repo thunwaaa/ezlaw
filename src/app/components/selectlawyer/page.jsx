e6a9c7faf262cfc7b5cc7277fe67e982b8194833
thunwachan <sittankunanit2008@gmail.com>
2024-11-16 03:59:50 +0700

    laws

--- src/app/components/selectlawyer/page.jsx
+++ src/app/components/selectlawyer/page.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
-
+import Link from 'next/link';
 
 function ChatButt() {
   return (
-    <div>
-      <button className='text-base bg-green-200 text-emerald-700 font-bold py-2 px-4 rounded-full
-        transition duration-300 delay-100 hover:bg-green-500 hover:text-white hover:delay-300'>Chat with Lawyer now!</button>
-    </div>
+    <button className='text-base bg-green-200 text-emerald-700 font-bold py-2 px-4 rounded-full
+        transition duration-300 delay-100 hover:bg-green-500 hover:text-white hover:delay-300'><Link href='/chat'>
+      Chat with Lawyer now!</Link>
+    </button>
   )
 }
-
 
 function SelectLawyer() {
   return (
     <div>
       <ul className='font-semibold text-2xl'>
-            <li className='flex p-10 '>
-                <div className='w-[120px] h-[120px] bg-black rounded-full mr-20'></div>
-                <div>
-                    <h2 className='cursor-pointer mt-2'>somkid</h2>
-                    <h3 className='text-sm'>description</h3>
-                </div>
-                <ChatButt />
-            </li>
-            <hr className='border-2 my-6 border-slate-300 opacity-30 ' />
-          </ul>
+        <li className='flex items-center p-10 justify-between'>
+          <div className='flex items-center'>
+            <div className='w-[120px] h-[120px] bg-black rounded-full mr-20'></div>
+            <div>
+              <h2 className='cursor-pointer mt-2'>somkid</h2>
+              <h3 className='text-sm'>description</h3>
+            </div>
+          </div>
+          <ChatButt />
+        </li>
+        <hr className='border-2 my-6 border-slate-300 opacity-30' />
+      </ul>
     </div>
   )
 }
--- src/app/consult/civillawyer/page.jsx
+++ src/app/consult/civillawyer/page.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
 import { NavigationMenuTrigger, NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuContent, NavigationMenuLink } from "@/components/ui/navigation-menu"
-
+import SelectLawyer from 'app/components/selectlawyer/page'
 
 function ConsultCivilLawyer() {
   return (
     <div className='bg-blue-200 p-10 h-screen'>
-      <div className='rounded-2xl mt-10 w-11/12 h-auto  bg-white  p-20 mx-10 flex justify-between text-left items-center'>
+      <div className='rounded-t-2xl mt-10 w-11/12 h-auto  bg-white pb-10 p-20 mx-10 flex justify-between text-left items-center'>
         <h1 className='font-extrabold text-5xl' >Lawyer</h1>
         <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
-            <NavigationMenuTrigger><Link href = '/consult/civillawyer'>แพ่งและพาณิชย์</Link></NavigationMenuTrigger>
+            <NavigationMenuTrigger>
+              <Link href = '/consult/civillawyer'>แพ่งและพาณิชย์</Link></NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className='flex-col w-48 text-left pl-4 pt-4 mb-5'>
                 <li><Link href = '/consult'>ทั้งหมด</Link></li>
@@ -22,6 +23,12 @@ function ConsultCivilLawyer() {
           
         </NavigationMenuList>
       </NavigationMenu>
+      
+      </div>
+
+      <div className='rounded-b-2xl w-11/12 h-auto  bg-white px-20 py-10 mx-10 text-left'>
+          <SelectLawyer />
+          <SelectLawyer />
       </div>
     </div>
   )
