ab5b719c3bdd05bc81fcdbee4cf50570f2f2fb08
thunwachan <sittankunanit2008@gmail.com>
2024-12-01 19:12:09 +0700

    edit law nav

--- src/app/components/navbar/NavBar.jsx
+++ src/app/components/navbar/NavBar.jsx
@@ -13,12 +13,23 @@ import { Playfair_Display } from "next/font/google";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { DialogHeader, DialogTitle , DialogDescription } from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
 
 const playfair_display = Playfair_Display({
     subsets: ['latin','latin-ext'],
     weight: ['400','700'],
 })
 
+const กฏหมาย = [
+    {
+      title: "กฏหมายอาญา",
+      href: "/crime/crime1"
+    },
+        {
+      title: "กฏหมายแพ่งและพาณิชย์",
+      href: "/civil"
+    }
+];
 
 export function NavBar() {
 
@@ -144,16 +155,11 @@ export function NavBar() {
                     <NavigationMenuTrigger className = 'py-5 text-xl hover:text-slate-500 hover:bg-none transition duration-300 content-center rounded-full'>กฏหมาย</NavigationMenuTrigger>
                     <NavigationMenuContent>
                         <ul className="  w-[100px] text-base  p-4 md:w-[200px] xl:w-[200px] ">
-                            <li className="mb-1">
-                                <Link href='/crime/crime1' className=" hover:text-slate-500 transition duration-300">
-                                    กฏหมายอาญา
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/civil' className=" hover:text-slate-500 transition duration-300">
-                                    กฏหมายแพ่งและพาณิชย์
-                                </Link>
-                            </li>
+                            {กฏหมาย.map((กฏหมาย, index) => (
+                                <li key={index}>
+                                    <Link href={กฏหมาย.href} className=" hover:text-slate-500 transition duration-300">{กฏหมาย.title}</Link>
+                                </li>
+                            ))}
                         </ul>
                     </NavigationMenuContent>
                 </NavigationMenuItem>
